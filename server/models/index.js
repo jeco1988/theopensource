cbfd104a88745b13dc34d2984e54e6f79f562520
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2023-02-20 21:20:44 +1030

    added models, config.js and server.js

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require("./User.js");
+const Languages = require("./Languages.js");
+const SavedGitHubRepo = require("./SavedGitHubRepo.js");
+const Comments = require("./Comments.js");
+
+module.exports = { User, Languages, SavedGitHubRepo, Comments };
