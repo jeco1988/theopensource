919b16dd9633ba18d61a8cd52b34604dc56a77ac
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2023-02-26 14:34:40 +1030

    added utils folder and apollo functions to client folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import FixedMenuLayout from './components/fixedmenulayout';
+import logo from "./logo.svg";
+import "./App.css";
+import FixedMenuLayout from "./components/fixedmenulayout";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <FixedMenuLayout></FixedMenuLayout>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <FixedMenuLayout></FixedMenuLayout>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
 
 export default function Login() {
   return (
-    <Container text style={{ marginTop: '7em' }}>
-      <Header as='h1'>LOGIN</Header>
-      <p>LOGINThis is the about page for our application.</p>
-      <p>You can use this page to provide information about your project, your team, or any other details you want to share with your users.</p>
+    <Container text style={{ marginTop: "7em" }}>
+      <Header as="h1">LOGIN</Header>
+      <p>LOGINhis is the about page for our application.</p>
+      <p>
+        You can use this page to provide information about your project, your
+        team, or any other details you want to share with your users.
+      </p>
     </Container>
   );
 }
