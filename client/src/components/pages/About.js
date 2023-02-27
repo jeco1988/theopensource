4860cb54e3d2da68f3d7233feb0f67b1317ff514
DaleDuiv <110407486+DaleDuiv@users.noreply.github.com>
2023-02-27 17:50:38 +1030

    added in aboutme content

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -11,22 +11,71 @@
 //   );
 // }
 import React from 'react';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Header, Card, Icon, Image } from 'semantic-ui-react';
 
 const About = () => {
   return (
     <>
     <Container text style={{ marginTop: '7em' }}>
       <Header as='h1'>About</Header>
-      <Segment>
-        <p>
-          This is an example app created using React and Semantic UI React. It demonstrates how to create a navigation bar using the `react-router-dom` library, and how to use the `useState` hook to manage state in a functional component.
-        </p>
-        <p>
-          To navigate the app, simply click on the links in the navigation bar at the top of the page.
-        </p>
-      </Segment>
-    </Container>
+      <Card.Group itemsPerRow={3}>
+        <Card>
+          <Image src={process.env.PUBLIC_URL + '/img/jack.jpg'} alt="Jack" wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Jack Economos</Card.Header>
+            <Card.Meta>
+            {/* <span className='date'>Joined in 2015</span> */}
+            </Card.Meta>
+            <Card.Description>
+              Hi, I'm Jack, I'm an up and coming Web Developer
+            </Card.Description>
+          </Card.Content>
+        <Card.Content extra>
+          <a href="https://github.com/jeco1988" target="blank_">
+          <Icon link name='gitlab'/>
+          My Github
+          </a>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Image src={process.env.PUBLIC_URL + '/img/shiham.jpg'} alt="Jack"  wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Shiham Jamaldeen</Card.Header>
+          <Card.Meta>
+          {/* <span className='date'>Joined in 2015</span> */}
+          </Card.Meta>
+          <Card.Description>
+            Hi, I'm Shiham, I'm an up and coming Web Developer
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://github.com/shiham-jamaldeen" target="blank_">
+          <Icon link name='gitlab'/>
+          My Github
+          </a>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Image src={process.env.PUBLIC_URL + '/img/dale.jpg'} alt="Jack" wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Dale Duivesteyn </Card.Header>
+          <Card.Meta>
+          {/* <span className='date'>Joined in 2015</span> */}
+          </Card.Meta>
+          <Card.Description>
+          Hi, I'm Dale, I'm an up and coming Web Developer
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://github.com/daleduiv" target="blank_">
+          <Icon link name='gitlab'/>
+          My Github
+          </a>
+        </Card.Content>
+      </Card>
+    </Card.Group>
+  </Container>
+    
     </>
   );
 };
