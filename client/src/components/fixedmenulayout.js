8498a9ba245242115b3d32c31ca3918104469f0d
DaleDuiv <110407486+DaleDuiv@users.noreply.github.com>
2023-03-03 01:35:34 +1030

    more responsive about page looks better in mobile first

--- client/src/components/fixedmenulayout.js
+++ client/src/components/fixedmenulayout.js
@@ -6,14 +6,10 @@ import About from './pages/About';
 import Donate from './pages/Donate';
 import Home from './pages/Home';
 import LoginForm from './loginform';
-import SignUpForm from './signupform'; //original signupform
-// import SignUpFormv2 from './drafts/signupform'
+import SignUpForm from './signupform'; 
 import Footer from './Footer';
 import {
   Container,
-  Image,
-  List,
-  Segment,
 } from 'semantic-ui-react';
 
 export default function FixedMenuLayout() {
@@ -26,8 +22,8 @@ export default function FixedMenuLayout() {
       
         <Navigation loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
 
-        <Container style={{ marginTop: '7em'}}>
-          <Routes style={{}}>
+        <Container style={{ marginTop: '7em', marginBottom: '7em', position: 'relative', minHeight: '100%'}}>
+          <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/donate" element={<Donate />} />
